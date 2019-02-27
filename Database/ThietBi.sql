fdcc44af52f564108573e533d71dbf85cc16ab59
Trương Vĩ Huy <45887563+truongvihuy@users.noreply.github.com>
2019-02-27 16:05:39 +0700

    Add files via upload

---
+++ Database/ThietBi.sql
@@ -0,0 +1,42 @@
+﻿---- Tạo database ----
+CREATE DATABASE ThietBi
+GO
+
+USE ThietBi
+GO
+
+CREATE TABLE ThietBi
+(
+	id CHAR(10) PRIMARY KEY,
+	ten NVARCHAR(100) NOT NULL DEFAULT N'Unknown',
+	gia INT NOT NULL DEFAULT 0,
+	soluong INT NOT NULL DEFAULT 0
+)
+GO
+CREATE TABLE KhachHang
+(
+	id CHAR(10) PRIMARY KEY,
+	ten NVARCHAR (100) NOT NULL DEFAULT N'Unknown',
+	diachi NVARCHAR(100) NULL,
+	sdt CHAR(10) NULL
+)
+GO
+CREATE TABLE HoaDon
+(
+	id CHAR(10) PRIMARY KEY,
+	idkhanghang CHAR(10) NOT NULL,
+	ngay DATETIME NOT NULL,
+	tongtien INT NOT NULL DEFAULT 0,
+	diachigiao NVARCHAR(100),
+	giaohang BIT
+)
+GO
+CREATE TABLE CTHoaDon
+(
+	id CHAR(10),
+	idthietbi CHAR(10),
+	soluong INT,
+
+	CONSTRAINT machitiet PRIMARY KEY (id,idthietbi)
+)
+GO
